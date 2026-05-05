83367bb57e056c4eeeb2a853e53524a0b8b06661
JosephShortt <g00419690@atu.ie>
2026-05-05 20:50:09 +0100

    server responds to different rquest paths and POST requests

--- httpServer/src/main/java/SimpleServer.java
+++ httpServer/src/main/java/SimpleServer.java
@@ -13,16 +13,18 @@ public class SimpleServer {
         String path;
         String version;
         Map<String, String> headers;
+        String body;
 
-        HttpRequest(String method, String path, String version, Map<String, String> headers){
+        HttpRequest(String method, String path, String version, Map<String, String> headers, String body){
             this.method = method;
             this.path = path;
             this.version = version;
             this.headers = headers;
+            this.body = body;
         }
 
         public String toString(){
-            return "Method: "+method + "\n" + "Path: "+path + "\n" + "Version: "+version + "\n" + "Headers: " +headers;
+            return "Method: "+method + "\n" + "Path: "+path + "\n" + "Version: "+version + "\n" + "Headers: " +headers + "\n" + "Body: "+body;
         }
     }
 
@@ -45,19 +47,82 @@ public class SimpleServer {
             headers.put(key,val);
         }
 
-        return new HttpRequest(method,path,version,headers);
+        String body = " ";
+        if(headers.containsKey("content-length")){
+            int contentLength = Integer.parseInt(headers.get("content-length"));
+            char[] bodyChars = new char[contentLength];
+            in.read(bodyChars, 0, contentLength);
+            body = new String(bodyChars);
+        }
+
+        return new HttpRequest(method,path,version,headers,body);
     }
 
     static void sendResponse(OutputStream out, int statusCode, String statusMessage, String body) throws IOException {
-        String reponse = "HTTP/1.1" + statusCode + " " + statusMessage + "\r\n" +
+        byte[] bodyBytes = body.getBytes("UTF-8");
+        String headers = "HTTP/1.1 " + statusCode + " " + statusMessage + "\r\n" +
                 "Content-Type: text/html\r\n" +
-                "Content-Length: " + body.length()+"\r\n" +
+                "Content-Length: " + bodyBytes.length+"\r\n" +
+                "Connection: close\r\n" +
                 "\r\n" +
                 body;
 
-        out.write(reponse.getBytes());
+        out.write(headers.getBytes("UTF-8"));
+        out.write(bodyBytes);
         out.flush();
     }
+
+    static void handleRequest(HttpRequest request, OutputStream out) throws IOException {
+        if(request.method.equals("GET")){
+            switch(request.path){
+                case "/":
+                    sendResponse(out, 200,"OK", "<html><body><h1>Welcome</h1><p>Home Page</p></body></html>");
+                    break;
+
+                case "/hello":
+                    sendResponse(out, 200,"OK", "<html><body><h1>Hello</h1><p>You hit the hello route</p></body></html>");
+                    break;
+
+                case "/about":
+                    sendResponse(out, 200,"OK", "<html><body><h1>About</h1><p>This is a raw Java HTTP server</p></body></html>");
+                    break;
+
+                default:
+                    sendResponse(out, 404,"Not Found", "<html><body><h1>404 - Not Found</h1><p>No route for "+ request.path + "</p></body></html>");
+                    break;
+            }
+        }
+        else if(request.method.equals("POST")){
+            switch (request.path){
+                case "/echo":
+                    sendResponse(out, 200, "OK", "<html><body><h1>ECHO</h1><p>You sent: </p>" + request.body + "</body></html>");
+                break;
+
+                default:
+                    sendResponse(out, 404, "Not Found", "<html><body><h1>404 -Not Found</h1></body></html>");
+                    break;
+            }
+        }
+        else{
+            sendResponse(out, 405, "Method Not Allowed","<html><body><h1>405 - Method Not Allowed</h1></body></html>");
+
+        }
+
+
+    }
+
+    static void handleClient(Socket client){
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            OutputStream out = client.getOutputStream();
+
+            HttpRequest request = parseRequest(in);
+        }
+        catch (IOException e){
+
+        }
+    }
+
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
@@ -73,9 +138,8 @@ public class SimpleServer {
         HttpRequest request = parseRequest(bf);
         System.out.println(request);
 
-        String body = "<html><body><h1>Hello from my java http server!</h1><p>Path: " +request.path + "</p></body></html>";
-        sendResponse(out, 200, "OK", body);
-
+        handleRequest(request, out);
+        System.out.println("Sending response...");
         client.close();
         serverSocket.close();
     }
