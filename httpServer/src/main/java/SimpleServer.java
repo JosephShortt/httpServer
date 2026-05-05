9f3daf8203a04403cdb4df5b6808be5e5534832b
JosephShortt <g00419690@atu.ie>
2026-05-05 19:24:33 +0100

    parse request line and headers

--- httpServer/src/main/java/SimpleServer.java
+++ httpServer/src/main/java/SimpleServer.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimpleServer {
 
@@ -9,21 +11,40 @@ public class SimpleServer {
         String method;
         String path;
         String version;
+        Map<String, String> headers;
 
-        HttpRequest(String method, String path, String version){
+        HttpRequest(String method, String path, String version, Map<String, String> headers){
             this.method = method;
             this.path = path;
             this.version = version;
+            this.headers = headers;
         }
 
         public String toString(){
-            return "Method: "+method + " | Path: "+path + " | Version: "+version;
+            return "Method: "+method + "\n" + "Path: "+path + "\n" + "Version: "+version + "\n" + "Headers: " +headers;
         }
     }
 
-    static HttpRequest parseRequestLine(String requestLine){
+
+    static HttpRequest parseRequest(BufferedReader in) throws IOException {
+        //parse request line
+        String requestLine = in.readLine();
         String[] parts = requestLine.split(" ");
-        return new HttpRequest(parts[0], parts[1], parts[2]);
+        String method = parts[0];
+        String path = parts[1];
+        String version = parts[2];
+
+        //parse headers
+        Map<String, String> headers = new HashMap<>();
+        String line;
+        while((line = in.readLine()) !=null && !line.isEmpty()){
+            int colonIndex = line.indexOf(":");
+            String key = line.substring(0,colonIndex).trim().toLowerCase();
+            String val = line.substring(colonIndex+1).trim();
+            headers.put(key,val);
+        }
+
+        return new HttpRequest(method,path,version,headers);
     }
 
     public static void main(String[] args) throws IOException {
@@ -36,15 +57,10 @@ public class SimpleServer {
 
         BufferedReader bf = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-        String requestLine = bf.readLine();
-        HttpRequest httpRequest = parseRequestLine(requestLine);
-        System.out.println(httpRequest);
 
+        HttpRequest request = parseRequest(bf);
+        System.out.println(request);
 
-        String line;
-        while((line = bf.readLine()) != null && !line.isEmpty()){
-            System.out.println(line);
-        }
 
         client.close();
         serverSocket.close();
